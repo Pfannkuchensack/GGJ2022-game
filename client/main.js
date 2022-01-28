fa36fbbe4baadc9e2d03ff8f1c8ae33f998dab20
Alexander Eichhorn <alex@code-with.us>
2022-01-29 00:44:41 +0100

    redis testen bitte
    Co-authored-by: Philipp Kübler <PKuebler@users.noreply.github.com>
    Co-authored-by: //ADB <adb-sh@users.noreply.github.com>

---
+++ client/main.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+function render() {
+    // render tiles
+
+
+
+    // render chars
+}
--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,33 @@ import './main.scss';
 import Game from "./Game";
 
 const io = require("socket.io-client");
-const socket = io(":5000/socket");
 
-socket.on("connect", () => {
-    console.log('connected', socket.id);
-});
-socket.on("disconnect", () => {
-    console.log('disconnected', socket.id);
-});
+window.addEventListener('load', ()=>{
+	'use strict';
 
-new Game({
-    canvas: document.getElementById('canvas'),
-    socket
-}).start();
+	var socket = io.connect('ws://localhost:8010', { reconnect: true, transports: ['websocket'], forceNew: true });
+	socket.on('connect', function (data) {
+		socket.emit('go', { gameid: 'test', charid: 'test1'});
+	});
+
+	socket.on('game', function (data) {
+		const json = JSON.parse(data);
+		console.log(data);
+	});
+
+	socket.on('debug', function (data) {
+		console.log(data);
+	});
+
+	document.getElementById('test').addEventListener('click', ()=>{
+		// alex hier spass haben darf
+		console.log('i clicked UwU');
+		socket.emit('game', { gameid: 'test', charid: 'test1'});
+	});
+
+	new Game({
+		canvas: document.getElementById('canvas'),
+		socket
+	}).start();
+
+});
