9d00585287d60b10529635bdf5c4693f25fa96bc
Alexander Eichhorn <alex@code-with.us>
2022-01-30 18:15:39 +0100

    try to work

--- client/src/app.ts
+++ client/src/app.ts
@@ -188,7 +188,7 @@ class App {
 }
 
 function startApp(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
-	const socket = io({ transports: ['websocket'], forceNew: true });
+	const socket = io({ transports: ['websocket', 'pulling'], forceNew: true });
 
 	const gameMap = new GameMap()
 	const app = new App(gameMap, socket, canvas, context);
